f5d42853b1c1f348493144150e645dbd08cfa6b2
Vincent-the-gamer <1127620453@qq.com>
2024-03-31 16:04:25 +0800

    chore: add developer functions.

--- contents/.vitepress/sidebars/zh_hans.ts
+++ contents/.vitepress/sidebars/zh_hans.ts
@@ -1,10 +1,17 @@
 export default [
     {
-        text: '内容',
+        text: '用户',
         collapsed: false,
         items: [
             { text: '说明', link: '/instruction' },
             { text: '功能', link: '/functions' },
+        ],
+    },
+    {
+        text: '开发者',
+        collapsed: true,
+        items: [
+            { text: '开发者功能', link: '/dev' }
         ],
     },
 ]
